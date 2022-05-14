5bf5a3266f3e9cf896221b7c33f92f5c8f2b55d5
Yauheni <eugene.kostroma@gmail.com>
2022-05-14 23:51:06 +0400

    lesson 6 complite

--- src/lessons/lesson6/lesson6.ts
+++ src/lessons/lesson6/lesson6.ts
@@ -1,4 +1,4 @@
-console.log('Lesson 6');
+console.log("Lesson 6");
 
 // Class
 // https://learn.javascript.ru/classes
@@ -12,6 +12,45 @@ console.log('Lesson 6');
 // Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего балла.
 // Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
 
+class Student {
+  constructor(
+    public name: string,
+    public surname: string,
+    public group: number,
+    public performance: number[]
+  ) {}
+  getAveragePerformance() {
+    return (
+      this.performance.reduce((acc, el) => acc + el) / this.performance.length
+    );
+  }
+  getSurnameAndGroup() {
+    return `${this.surname} ${this.group}`;
+  }
+}
+
+const studentGroup = [
+  new Student("Egor", "Kukusha", 233, [1, 2, 3, 3, 5]),
+  new Student("Artem", "Topol", 234, [4, 3, 3, 3, 5]),
+  new Student("Jora", "Bereza", 233, [2, 5, 5, 3, 5]),
+  new Student("Vadim", "Asip", 234, [1, 2, 3, 4, 5]),
+  new Student("Andrey", "ASDdsdsd", 236, [4, 5, 5, 5, 4]),
+  new Student("Vika", "Asdasdasdasd", 233, [1, 2, 3, 3, 2]),
+  new Student("Anna", "Adasdasdasd", 235, [1, 2, 4, 3, 4]),
+  new Student("Nastya", "Asdasdasdads", 235, [1, 4, 3, 5, 4]),
+  new Student("Semen", "Addddsdssds", 236, [5, 4, 3, 3, 2]),
+  new Student("Alexandr", "Adsdffffdss", 233, [1, 2, 5, 3, 5]),
+];
+console.log(
+  studentGroup.sort(
+    (a, b) => a.getAveragePerformance() - b.getAveragePerformance()
+  )
+);
+studentGroup.forEach((el) => {
+  if (el.performance.every((el) => el >= 4))
+    console.log(el.getSurnameAndGroup());
+});
+
 // Task 02
 // Создать класс с двумя переменными. Добавить конструктор с входными параметрами и инициализирующий члены класса по умолчанию.
 // Можно ли создать метод на экземпляре класса который будет удалять сам экземпляр класса?
@@ -24,17 +63,132 @@ console.log('Lesson 6');
 // Создать метод выводящий время в строке формата HH:MM:SS
 // Создать класс по вышеуказанному описанию
 
+class Time {
+  constructor(private date: Date) {}
+
+  public set time(time: string) {
+    let arrTime = time.split(":");
+    this.date.setHours(+arrTime[0]);
+    this.date.setMinutes(+arrTime[1]);
+    this.date.setSeconds(+arrTime[2]);
+  }
+  public get time() {
+    return this.date.toLocaleTimeString();
+  }
+
+  public set hours(h: number) {
+    if (h >= 0 && h <= 23) this.date.setHours(h);
+  }
+  public set minutes(m: number) {
+    if (m >= 0 && m <= 59) this.date.setMinutes(m);
+  }
+  public set seconds(s: number) {
+    if (s >= 0 && s <= 59) this.date.setSeconds(s);
+  }
+}
+
+let z = new Time(new Date());
+console.log(z.time);
+z.time = "23:12:01";
+console.log(z.time);
+z.hours = 13;
+z.minutes = 26;
+z.seconds = 12;
+console.log(z.time);
+
 // Task 04
 // Класс Покупатель: Фамилия, Имя, Адрес, Номер банковского счета;
 // Методы: установка значений атрибутов, получение значений атрибутов, вывод информации.
 // Создать массив объектов данного класса.
 // Вывести список покупателей в алфавитном порядке и список покупателей, у которых номер кредитной карточки находится в заданном диапазоне.
 
+class Customers {
+  constructor(
+    private nameV: string,
+    private surnameV: string,
+    private locationV: string,
+    private cardV: string
+  ) {}
+
+  public get name(): string {
+    return this.nameV;
+  }
+  
+  public set name(n : string) {
+    this.nameV = n;
+  }
+  
+  public get surname(): string {
+    return this.surnameV;
+  }
+  
+  public set surname(s : string) {
+    this.surnameV = s;
+  }
+  
+  public get location(): string {
+    return this.locationV;
+  }
+  
+  public set location(l : string) {
+    this.locationV = l;
+  }
+  
+  public get card(): string {
+    return this.cardV;
+  }
+  
+  public set card(c : string) {
+    this.cardV = c;
+  }
+  
+  public get cardInfo(){
+    return `name:${this.name} surname:${this.surname} location: ${this.location} cardNumber: ${this.card}`
+  }
+}
+
 // Task 05
 // Создать класс машина - имеющий марку, число цилиндров, мощность. Определить конструктор и функцию печати.
 // Создать производный класс – грузовик, имеющий грузоподъемность кузова.
 // Определить функции переназначения марки и грузоподъемности.
 
+
+class Car{
+  constructor(
+    protected brand:string,
+    protected engine:number,
+    protected power:number,
+  ){}
+    public get car(){
+      return `brand is ${this.brand}, engine has ${this.engine} steams, horse power is ${this.power}`
+    }
+}
+
+class Track extends Car{
+  constructor(protected brand:string,
+    protected engine:number,
+    protected power:number,
+    protected weightLiftPower:number){
+    super(brand,engine,power)
+  }
+  public set newBrand(nB:string){
+    this.brand=nB
+  }
+  public get weight(){
+    return this.weightLiftPower
+  }
+  public set weight(w:number){
+    this.weightLiftPower=w
+  }
+}
+
+const heavy = new Track('MotorTrack',6,100500,99999)
+console.log(heavy.car);
+heavy.weight = 1
+console.log(heavy.weight);
+
+
+
+
 // just a plug
-export default () => {
-};
+export default () => {};
